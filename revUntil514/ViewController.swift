de4300c905ed25a28f61165415e79e012b5b38ba
huro3h_satoshi <huro3h@gmail.com>
2016-05-14 12:46:02 +0800

    準備 info.plist secVC追加

--- revUntil514/ViewController.swift
+++ revUntil514/ViewController.swift
@@ -1,10 +1,8 @@
-//
 //  ViewController.swift
 //  revUntil514
 //
 //  Created by satoshiii on 2016/05/14.
 //  Copyright © 2016年 satoshiii. All rights reserved.
-//
 
 import UIKit
 
@@ -12,14 +10,25 @@ class ViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
-	}
-
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
 	}
 
 
+	
+	
+	
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+//	override func didReceiveMemoryWarning() {
+//		super.didReceiveMemoryWarning()
+//		// Dispose of any resources that can be recreated.
+//	}
 }
 
---
+++ revUntil514/secViewController.swift
@@ -0,0 +1,39 @@
+//  secViewController.swift
+//  revUntil514
+//
+//  Created by satoshiii on 2016/05/14.
+//  Copyright © 2016年 satoshiii. All rights reserved.
+
+import UIKit
+
+class secViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
+}
