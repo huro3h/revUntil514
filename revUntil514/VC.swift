aa8cbd8b9b33ee5743b1334deda39a7ced39531b
huro3h_satoshi <huro3h@gmail.com>
2016-05-17 16:11:27 +0800

    appendの所でエラーが出た理由 Value of type '[String : String]' has no member 'append' 辞書型のデータはappendでは入れられないとのエラー [[ ]]にすることで配列の中に１個だけ辞書が入った状態になっている

--- revUntil514/VC.swift
+++ revUntil514/VC.swift
@@ -9,21 +9,28 @@ class VC: UIViewController {
 
 	@IBOutlet weak var txttitle: UITextField!
 	@IBOutlet weak var txtday: UITextField!
-	@IBOutlet weak var txtcatgory: UITextField!
+	@IBOutlet weak var txtcategory: UITextField!
 	@IBOutlet weak var txtarticle: UITextView!
 	@IBOutlet weak var btnSave: UIButton!
 	
-	var articleList = [["title":"a1"]]
+	var articleList = [["title":"今日の出来事"]]
 
 	override func viewDidLoad() {
         super.viewDidLoad()
-
+		print(articleList)
     }
 
 	@IBAction func tapBtnSave(sender: UIButton) {
-	
+		
+		// appendは配列の時のみ使用可能
+		// [[ ]]にすることで配列の中に１個だけ辞書が入った状態になっている
 		articleList.append(["title":txttitle.text!])
 		print(articleList)
+		
+		var myDefault = NSUserDefaults.standardUserDefaults()
+		myDefault.setObject(articleList, forKey: "articleList")
+		myDefault.synchronize()
+	
 	}
 	
 	
